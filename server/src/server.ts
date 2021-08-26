f5d0d49d210626c076cfba4cfd8a1696e272ac08
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2021-08-26 22:30:01 +0545

    Add test api

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,19 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
+
+const users: { name: string, email: string, password: string }[] = []
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', (req, res) => {
+    const user = { name: req.body.name, email: req.body.email, password: req.body.password }
+    users.push(user)
+})
+
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`)
 })
